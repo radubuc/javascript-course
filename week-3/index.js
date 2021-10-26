124281a601a1c2d8da77f82fa92e14e3ab17cc33
Renee Dubuc <rdubuc@GYKantler.com>
2021-10-26 19:03:40 -0400

    Added week 3 class presentation and Object video notes in practice js file

--- week-3/index.js
+++ week-3/index.js
@@ -60,52 +60,75 @@
 
 
 
-//INTERMEDIATE ARRAY METHODS VIDEO NOTES
+////INTERMEDIATE ARRAY METHODS VIDEO NOTES
 
-//Map - Create a new array that has lengths of each string in array
-//Passes in each param 1x then returns how many characters in each name in new array
-//Parameter can be anything, doesn't have to be element but its a good descriptor
-//NOTE: Be very careful with () and {} here! See lines 70-72 
-let names = ['Sam', 'Tom', 'Eric', 'Sally', 'Nicholas'];
-let lengths = names.map(function(element) {
-    return element.length; //Returns how many elements in array
-});
-console.log(lengths); //returns how many characters in each string
-
-
-//Reduce - Takes all values in array and reduces them down to one value. Final value is result of a reducer function that we pass in as an argument
-//Finding sum of lengths array AKA sum of all the characters in all the names
-//Accumulator accumulates all values together, and it's result of the previously returned value. Each time function executes, accumulator will be whatever was returned by the previous execution
-//CurrentValue or element in array
-let sum = lengths.reduce(function(accumulator, currentValue) {
-    return accumulator + currentValue;
-});
-console.log(sum);
-
-//Ex with initial value for the accumulator to start at
-let sum2 = lengths.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue;
-}, 10);
-console.log(sum2);
+////Map - Create a new array that has lengths of each string in array
+////Passes in each param 1x then returns how many characters in each name in new array
+////Parameter can be anything, doesn't have to be element but its a good descriptor
+////NOTE: Be very careful with () and {} here! See lines 70-72 
+//let names = ['Sam', 'Tom', 'Eric', 'Sally', 'Nicholas'];
+//let lengths = names.map(function(element) {
+//    return element.length; //Returns how many elements in array
+//});
+//console.log(lengths); //returns how many characters in each string
 
 
-//For Each method (not loop) - Invokes a functions for each element in array, but doesn't return new array from results of invocation
-//Log out each element (name) in array without loop
-names.forEach(function (element) {
-    console.log(element);
-});
+////Reduce - Takes all values in array and reduces them down to one value. Final value is result of a reducer function that we pass in as an argument
+////Finding sum of lengths array AKA sum of all the characters in all the names
+////Accumulator accumulates all values together, and it's result of the previously returned value. Each time function executes, accumulator will be whatever was returned by the previous execution
+////CurrentValue or element in array
+//let sum = lengths.reduce(function(accumulator, currentValue) {
+//    return accumulator + currentValue;
+//});
+//console.log(sum);
+
+////Ex with initial value for the accumulator to start at
+//let sum2 = lengths.reduce(function (accumulator, currentValue) {
+//    return accumulator + currentValue;
+//}, 10);
+//console.log(sum2);
 
 
-//Filter - Returns boolean value on each element in array. Returns new array that contains all elements where invoked method returns true. Use to filter out specific elements and only keep ones that pass boolean expression.
-//Filter for names with even number of characters s
-let evens = names.filter(function (element) {
-    return element.length % 2 == 0;
-});
-console.log(evens);
+////For Each method (not loop) - Invokes a functions for each element in array, but doesn't return new array from results of invocation
+////Log out each element (name) in array without loop
+//names.forEach(function (element) {
+//    console.log(element);
+//});
+
+
+////Filter - Returns boolean value on each element in array. Returns new array that contains all elements where invoked method returns true. Use to filter out specific elements and only keep ones that pass boolean expression.
+////Filter for names with even number of characters s
+//let evens = names.filter(function (element) {
+//    return element.length % 2 == 0;
+//});
+//console.log(evens);
 
 
 
-//Splice - Alters content of array by adding, modifying, or removing element from array at specific location. Most common is to remove element
-//Remove element (name) from array and return as an array
-let removedElement = names.splice(1, 1); //(Location to start, how many elements to remove)
-console.log(removedElement);
+////Splice - Alters content of array by adding, modifying, or removing element from array at specific location. Most common is to remove element
+////Remove element (name) from array and return as an array
+//let removedElement = names.splice(1, 1); //(Location to start, how many elements to remove)
+//console.log(removedElement);
+
+
+
+
+//OBJECTS VIDEO NOTES
+//Object literal
+var dvdPlayer = {
+    height: 3, //properties
+    width: 18, 
+    depth: 12, 
+    weight: 7, 
+    color: 'black',
+    dvdName: 'Mulan',
+
+    //Adding a method
+    printDVDName: function () {
+        console.log(this.dvdName);
+        //"This" refers to object itself. Since dvdName isn't var outside of object, we're calling it as a property inside the object. 
+    }
+}
+
+console.log(dvdPlayer.depth);
+dvdPlayer.printDVDName(); //Can't return "Mulan" w/o "this" keyword
