a267846c3463d38b6333a07e0502d08e38b496c7
Renee Dubuc <rdubuc@GYKantler.com>
2021-10-28 23:46:58 -0400

    Started week 3 hw and added openclass arrays and functions practice files

---
+++ week-3/hw/app.js
@@ -0,0 +1,16 @@
+//1a. Subtract first element of array from last
+var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+//console.log((ages[ages.length - 1]) - ages[0]);
+
+//1b. Added new age to array and repeat 1a
+var ages2 = [3, 9, 23, 64, 2, 8, 28, 93, 5];
+//console.log((ages2[ages2.length - 1]) - ages2[0]);
+
+//1c. Loop through array and calculate avg age
+for (age of ages2) {
+
+}
+
+function calcAvgAge() {
+    let avg = 
+}
