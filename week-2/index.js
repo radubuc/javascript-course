8a1ce71858a3f01acb313f9c390f5fcfefc2b5f4
Renee Dubuc <rdubuc@GYKantler.com>
2022-08-29 10:32:54 -0400

    Finished openclass week 2 loops notes

--- week-2/index.js
+++ week-2/index.js
@@ -1,74 +1,74 @@
-////////LOOPS VIDEO NOTES
-//////Variable declaration
-////var firstName = "sam";
+//LOOPS VIDEO NOTES
+//Variable declaration
+// var firstName = "sam";
 
-//////If statement
-////if (firstName == "sam") {
-////    console.log("Hello, Sam!");
-////}
+//If statement
+// if (firstName == "sam") {
+//    console.log("Hello, Sam!");
+// }
 
-//////For Loop
-////for (let i = 0; i < 10; i++) {
-////    console.log(i);
-////}
+//For Loop
+// for (let i = 0; i < 10; i++) {
+//    console.log(i);
+// }
 
-////console.log("______________________");
+//console.log("______________________");
 
-//////For Loop with nested If statement
-////for (let i = 0; i < 20; i++) {
-////    if (i % 2 == 0) {
-////        console.log(i);
-////    }
-////}
+//For Loop with nested If statement
+//for (let i = 0; i < 20; i++) {
+//    if (i % 2 == 0) {
+//        console.log(i);
+//    }
+//}
 
-////console.log("______________________");
+//console.log("______________________");
 
-//////Array of names
-////let names = ["Sam", "Tom", "Hillary"];
+//Array of names
+//let names = ["Sam", "Tom", "Hillary"];
 
-//////For Loop iterating through names array 1x
-////for (let i = 0; i < names.length; i++) {
-////    console.log(names[i]);
-////}
+//For Loop iterating through names array 1x
+//for (let i = 0; i < names.length; i++) {
+//    console.log(names[i]);
+//}
 
-////console.log("______________________");
+//console.log("______________________");
 
-//////While Loop
-////let a = 0;
+//While Loop
+//let a = 0;
 
-////while (a < 10) {
-////    console.log(a);
-////    a++;
-////}
+//while (a < 10) {
+//    console.log(a);
+//    a++;
+//}
 
-////console.log("______________________");
+//console.log("______________________");
 
-//////Do While Loop
-////let b = 110;
+//Do While Loop
+//let b = 110;
 
-////do {
-////    console.log(b);
-////} while (b < 10);
+//do {
+//    console.log(b);
+//} while (b < 10);
 
-////console.log("______________________");
+//console.log("______________________");
 
-//////Modified For Loops
-////    //For In Loop - self-iterates through the index
-////    for (i in names) {
-////        console.log(i);
-////    }
+//Modified For Loops
+//    //For In Loop - self-iterates through the index
+//    for (i in names) {
+//        console.log(i);
+//    }
 
-////    console.log("______________________");
+//    console.log("______________________");
 
-////    //For Of Loop - self-iterates through the object/value
-////    //This is similar to For Each loop in Java?
-////    for (name of names) {
-////        console.log(name);
-////    }
+//    For Of Loop - self-iterates through the object/value
+//    This is similar to For Each loop in Java?
+//    for (name of names) {
+//        console.log(name);
+//    }
 
 /////////////////////////////////////////////////////////////////////////////
 
-////USER INPUT VIDEO NOTES
+//USER INPUT VIDEO NOTES
 //var name = window.prompt("What is your name?");
 //window.alert(`Welcome, ${name}`);
 
